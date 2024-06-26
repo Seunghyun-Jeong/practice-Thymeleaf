926a2a6d39aa64f1de105a8122e4a1145bdeedfc
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-06-26 22:18:21 +0900

    feat: Model 추가

---
+++ src/main/java/com/example/demo/controller/ThymeleafController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+    @GetMapping("show")
+    public String showView(Model model) {
+        //Model에 데이터 추가
+        model.addAttribute("name", "정승현");
+
+        //반환값으로 뷰 이름을 설정
+        return "useThymeleaf";
+    }
+}
